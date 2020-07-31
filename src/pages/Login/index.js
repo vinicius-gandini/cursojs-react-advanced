847d42c6ea12a2b57db70536bba4753580d19309
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-07-30 22:37:19 -0300

    Iniciado desenvolvimento do projeto com Styled Components :ok_hand:

---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from '../../styles/globalStyles';
+
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Eae</small>
+      </Title>
+      <Paragrafo>fisdjgjksgjs kgskjdgsdjgs gskdgjsdk</Paragrafo>
+    </Container>
+  );
+}
