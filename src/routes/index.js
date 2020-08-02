d583009b2965d79bd536e0eb2f83ba4ee2c212d8
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-08-02 00:24:45 -0300

    Adicionado Toastify para lançar mensagens :ok_hand:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+// import { toast } from 'react-toastify';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
@@ -8,7 +9,7 @@ import Page404 from '../pages/Page404';
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute exact path="/" component={Login} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
