6b3492e9173aea6ab29e4bab46500ba326b63a7c
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-08-22 22:03:01 -0300

    feat: instalado Redux para gerenciar estados globais :ok_hand:

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { Container } from '../../styles/globalStyles';
+import { useDispatch } from 'react-redux';
 
+import { Container } from '../../styles/globalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
+
   return (
     <Container>
       <Title>
@@ -11,7 +22,9 @@ export default function Login() {
         <small>Eae</small>
       </Title>
       <Paragrafo>fisdjgjksgjs kgskjdgsdjgs gskdgjsdk</Paragrafo>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
