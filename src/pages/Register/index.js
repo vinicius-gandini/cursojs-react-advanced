87ee5b121c724a30970ab7b647b6a15933f66bcf
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-08-27 00:58:55 -0300

    feat: confeccionada página de registro de usuário :ok_hand:

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { get } from 'lodash';
+import axios from '../../services/axios';
+import history from '../../services/history';
 
 import { Container } from '../../styles/globalStyles';
+import { Form } from './styled';
 
 export default function Register() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error('Nome deve ter entre 3 e 255 caracteres');
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha deve ter entre 6 e 50 caracteres');
+    }
+
+    if (formErrors) return;
+
+    try {
+      await axios.post('/users', {
+        nome,
+        password,
+        email,
+      });
+      toast.success('Cadastro realizado');
+      history.push('/');
+    } catch (err) {
+      const errors = get(e, 'response.data.errors', []);
+
+      errors.map((error) => toast.error(error));
+    }
+  }
+
   return (
     <Container>
-      <h1>Regiser</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeHolder="Seu nome"
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeHolder="Seu e-mail"
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeHolder="Sua senha"
+          />
+        </label>
+
+        <button type="submit">Criar minha conta</button>
+      </Form>
     </Container>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -18,8 +18,8 @@ export default function Routes() {
       <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
       <MyRoute exact path="/aluno/" component={Aluno} isCLosed />
       <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
-      <MyRoute exact path="/login/" component={Register} isClosed={false} />
-      <MyRoute exact path="/register/" component={Login} isClosed={false} />
+      <MyRoute exact path="/login/" component={Login} isClosed={false} />
+      <MyRoute exact path="/register/" component={Register} isClosed={false} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
