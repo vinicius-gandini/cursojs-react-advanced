32f6dce4303343a58d4674d1ad7ab251ddee7e6b
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-09-03 01:08:38 -0300

    feat: criada verificação do usuário logado, para salvar os dados alterados :ok_hand:

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -28,9 +28,47 @@ function persistRehydrate({ payload }) {
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
-function registerRequest({ payload }) {
+// eslint-disable-next-line consistent-return
+function* registerRequest({ payload }) {
   const { id, nome, email, password } = payload;
-  console.log('continua...');
+
+  try {
+    if (id) {
+      yield call(axios.put, '/users', {
+        email,
+        nome,
+        password: password || undefined,
+      });
+      toast.success('Dados alterados com sucesso');
+      yield put(actions.registerUpdatedSuccess({ nome, email, password }));
+    } else {
+      yield call(axios.post, '/users', {
+        email,
+        nome,
+        password,
+      });
+      toast.succes('Conta criada com sucesso');
+      yield put(actions.registerCreatedSuccess({ nome, email, password }));
+      history.push('/login');
+    }
+  } catch (e) {
+    const errors = get(e, 'response.data.errors', []);
+    const status = get(e, 'response.status', 0);
+
+    if (status === 401) {
+      toast.error('Voce precisa fazer login novamente');
+      yield put(actions.loginFailure());
+      return history.push('/login');
+    }
+
+    if (errors.length > 0) {
+      errors.map((error) => toast.error(error));
+    } else {
+      toast.error('Erro desconhecido');
+    }
+
+    yield put(actions.registerFailure());
+  }
 }
 
 export default all([
