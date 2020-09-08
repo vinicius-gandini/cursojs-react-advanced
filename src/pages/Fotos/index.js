38a0687e9758b25b3b3f219a800daeb388a0f1f9
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-09-07 23:24:17 -0300

    feat: adicionada função de exibir e adicionar foto de perfil do Aluno :ok_hand:

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { get } from 'lodash';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import * as actions from '../../store/modules/auth/actions';
 
 import { Container } from '../../styles/globalStyles';
+import Loading from '../../components/Loading';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
 
-export default function Fotos() {
+export default function Fotos({ match }) {
+  const dispatch = useDispatch();
+  const id = get(match, 'params.id', '');
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [foto, setFoto] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        setFoto(get(data, 'Fotos[0].url', ''));
+        setIsLoading(false);
+      } catch (err) {
+        toast.error('Erro ao obter imagem');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+
+    getData();
+  }, [id]);
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const fotoURL = URL.createObjectURL(file);
+
+    setFoto(fotoURL);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('foto', file);
+
+    try {
+      setIsLoading(true);
+
+      await axios.post('/fotos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Foto enviada com sucesso');
+
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const { status } = get(err, 'response.status', 0);
+      toast.error('Erro ao enviar foto');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
-      <h1>Fotos</h1>
+      <Loading isLoading={isLoading} />
+      <Title>Fotos</Title>
+
+      <Form>
+        <label htmlFor="foto">
+          {foto ? <img src={foto} alt="foto" /> : 'Selecionar'}
+          <input type="file" id="foto" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
